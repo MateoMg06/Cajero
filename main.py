4ce500a670105f9cb781a034984bc3fecc7c772b
mateo <mateomercado234@gmail.com>
2026-02-27 14:29:22 -0500

    funcion retirar

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ print("-----------CAJERO AUTOMATICO--------------" \
 "2.retirar dinero" \
 "3.Depositar Dinero" \
 "4.salir")
+
+if(menu==1):
+    print("su saldo es ",saldo_inicial)
+
+elif(menu==2):
+    monto_retirar=int(input("Cuanto desea retirar"))
+if (monto_retirar>saldo_inicial):
+    print("fondos insuficientes")
+elif(monto_retirar<=saldo_inicial):
+    saldo_nuevo=monto_retirar-saldo_inicial
+    print("su saldo ahora es: ",saldo_nuevo)
+else:
+    print("digite un monto valido")
+
+
+
+
+
+
+

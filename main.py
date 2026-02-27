6ce39d16fb19687b6fa377089df8a4e7eae39ff4
mateo <mateomercado234@gmail.com>
2026-02-27 14:32:35 -0500

    agregando funcion for

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 saldo_inicial=1000
 op=int(input("Cuanto operaciones desea realizar"))
+for i in range(op)
 menu=int(input("digite que desar realizar"))
 print("-----------CAJERO AUTOMATICO--------------" \
 "1. consultar saldo" \
 "2.retirar dinero" \
 "3.Depositar Dinero" \
 "4.salir")
+
 
 
 

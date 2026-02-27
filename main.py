e8244f536085f6a71555883a80f46dead66005d6
mateo <mateomercado234@gmail.com>
2026-02-27 14:18:37 -0500

    correciones#3

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+saldo_inicial=1000
+op=int(input("Cuanto operaciones desea realizar"))
+menu=int(input("digite que desar realizar"))
+print("-----------CAJERO AUTOMATICO--------------" \
+"1. consultar saldo" \
+"2.retirar dinero" \
+"3.Depositar Dinero" \
+"4.salir")
 
 if(menu==1):
     print("su saldo es ",saldo_inicial)

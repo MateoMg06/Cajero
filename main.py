55e8976f9c2606f0112f8d335c4b7e27ca87eef1
mateo <mateomercado234@gmail.com>
2026-02-28 00:20:50 -0500

    correcionretirar

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ print("2. retirar dinero")
 print("3. Depositar Dinero") 
 print("4. salir ")
 
-if(menu==1):
-    print("su saldo es: ",saldo_inicial) 
+if menu == 2:
+    while True:
+        retiro = int(input("Digite el valor a retirar: "))
+        if retiro <= saldo_inicial:
+            saldo_inicial -= retiro
+            print("Retiro exitoso. Saldo actual:", saldo_inicial)
+            break
+        else:
+            print("Saldo insuficiente, intente otra vez") 
+
+
     
 
 

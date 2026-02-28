fe3b468c86fced7f431fc4aa6cb589b96ffe99f9
mateo <mateomercado234@gmail.com>
2026-02-28 00:28:02 -0500

    correcionretirar

--- main.py
+++ main.py
@@ -18,38 +18,9 @@ if menu == 2:
             print("Retiro exitoso. Saldo actual:", saldo_inicial)
             break
         else:
-            print("Saldo insuficiente, intente otra vez")  
-
-
-    
-
+            print("Saldo insuficiente, intente otra vez")   
 
     
-
-
-
-op=int(input("Cuanto operaciones desea realizar"))
-for i in range(op):
-    menu=int(input("digite que desar realizar"))
-print("-----------CAJERO AUTOMATICO--------------" \
-"1. consultar saldo" \
-"2.retirar dinero" \
-"3.Depositar Dinero" \
-"4.salir")
-
-if(menu==1):
-    print("su saldo es ",saldo_inicial)
-
-elif(menu==2):
-    monto_retirar=int(input("Cuanto desea retirar"))
-if (monto_retirar>saldo_inicial):
-    print("fondos insuficientes")
-elif(monto_retirar<=saldo_inicial):
-    saldo_nuevo=monto_retirar-saldo_inicial
-    print("su saldo ahora es: ",saldo_nuevo)
-else:
-    print("digite un monto valido")
-
 
 
 

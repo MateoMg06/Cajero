48e2f2d2090d4478263817b9728cd8c3aa80fe0e
mateo <mateomercado234@gmail.com>
2026-02-28 09:41:23 -0500

    re

--- main.py
+++ main.py
@@ -5,50 +5,19 @@ for i in range(op):
     menu=int(input("digite que desar realizar "))
    
 print("-----------CAJERO AUTOMATICO--------------")
-print("1. consultar saldo ") 
-print("2. retirar dinero ") 
-print("3. Depositar Dinero ")
-print("4. salir ")
+print("1. consultar saldo") 
+print("2. retirar dinero") 
+print("3. Depositar Dinero") 
+print("4. salir ") 
 
-
-
-            
-            
-<<<<<<< HEAD
-=======
-if menu == 3:
-    while True:
-        monto_depositar = int(input("Digite qué monto desea depositar: "))
-
-        if monto_depositar <= 0:
-            print("Error, digite el monto nuevamente")
-        else:
-            saldo_inicial += monto_depositar
-            print("Depósito exitoso, su saldo actual es:", saldo_inicial)
-            break    
-
-
-
-
-
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-  
->>>>>>> depositar
-            
+    
  
+    
+  
+
 
 
     
-
 
 
 

1ce0d8e984624b47ae76982f5ad30300e744cd48
mateo <mateomercado234@gmail.com>
2026-02-27 23:19:46 -0500

    mejoras

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 saldo_inicial=1000
-op=int(input("Cuanto operaciones desea realizar"))
+op=int(input("Cuanto operaciones desea realizar "))
 for i in range(op):
-    menu=int(input("digite que desar realizar"))
-print("-----------CAJERO AUTOMATICO--------------" \
-"1. consultar saldo" \
-"2.retirar dinero" \
-"3.Depositar Dinero" \
-"4.salir")
+   
+ print("-----------CAJERO AUTOMATICO--------------")
+print("1. consultar saldo") 
+print("2. retirar dinero") 
+print("3. Depositar Dinero") 
+print("4. salir ")
+
+menu=int(input("digite que desar realizar "))
+
 
 
     
